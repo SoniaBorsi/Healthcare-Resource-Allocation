f11a559aa65bed6de920b39f150d7b9ab6c5d5cd
Sonia Borsi <soniaborsi1101@gmail.com>
2024-05-21 09:05:08 +0200

    changed dataset function

--- get_datasets.py
+++ get_datasets.py
@@ -4,6 +4,10 @@ import dask.dataframe as dd
 import tempfile
 from io import StringIO
 
+import requests
+import tempfile
+import dask.dataframe as dd
+
 def get_hospitals_series_id():
     url = "https://myhospitalsapi.aihw.gov.au/api/v1/datasets/"
     headers = {
@@ -18,29 +22,28 @@ def get_hospitals_series_id():
             f.write(response.text)
         datasets = dd.read_csv("datasets.csv")
         hospitals_series_id = datasets['DataSetId'].compute()
-        return hospitals_series_id
+        return datasets, hospitals_series_id
     else:
         print("Failed to fetch data. Status code:", response.status_code)
-        return None
+        return None, None
 
-hospitals_series_id = get_hospitals_series_id()
-if hospitals_series_id is not None:
-    print(hospitals_series_id)
+def filter_dataset_ids(datasets):
+    filtered_datasets = datasets[datasets['ReportedMeasureName'] == 'all patients']
+    filtered_ids = filtered_datasets['DataSetId'].compute()
+    return filtered_ids
 
-
-def download_datasets(num_datasets_to_download, dataset_ids = hospitals_series_id):
+def download_datasets(num_datasets_to_download, dataset_ids):
     base_url = "https://myhospitalsapi.aihw.gov.au/api/v1/datasets/"
     headers = {
-    'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
-    'User-Agent': 'MyApp/1.0',
-    'accept': 'text/csv'
+        'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+        'User-Agent': 'MyApp/1.0',
+        'accept': 'text/csv'
     }
     for dataset_id in dataset_ids[:num_datasets_to_download]:
         url = f"{base_url}{dataset_id}/data-items"
         response = requests.get(url, headers=headers)
 
         if response.status_code == 200:
-
             with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
                 temp_file.write(response.text)
                 temp_file_path = temp_file.name
@@ -49,13 +52,17 @@ def download_datasets(num_datasets_to_download, dataset_ids = hospitals_series_i
 
             print(f"Dataset ID: {dataset_id}")
             print(ddf.head())
-
         else:
             print(f"Error fetching dataset with ID {dataset_id}:")
             print("Status Code:", response.status_code)
             print("Response Headers:", response.headers)
             print("Response Text:", response.text)
 
+datasets, hospitals_series_id = get_hospitals_series_id()
 
-num_datasets_to_download = 5
-download_datasets(num_datasets_to_download) 
+if hospitals_series_id is not None:
+    filtered_dataset_ids = filter_dataset_ids(datasets)
+    num_datasets_to_download = 5
+    download_datasets(num_datasets_to_download, filtered_dataset_ids)
+else:
+    print("No dataset IDs to process.")
