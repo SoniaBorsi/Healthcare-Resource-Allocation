e4b43171b1aff7f6013d6aff2d267f3b7aaf4f17
Sonia Borsi <soniaborsi1101@gmail.com>
2024-05-23 11:56:44 +0200

    updated data collector

---
+++ data_collector/get_datasets.py
@@ -0,0 +1,185 @@
+import psycopg2
+import requests
+import tempfile
+import pandas as pd
+import dask.dataframe as dd
+from pyspark.sql import SparkSession
+import findspark
+import psycopg2 
+from sqlalchemy import create_engine 
+
+
+#with dask 
+
+#FUCTION TO EXTRACT ALL THE IDS
+# def get_hospitals_series_id():
+#     url = "https://myhospitalsapi.aihw.gov.au/api/v1/datasets/"
+#     headers = {
+#         'Authorization': 'Bearer YOUR_ACCESS_TOKEN',  
+#         'User-Agent': 'MyApp/1.0',
+#         'accept' : 'text/csv'
+#     }
+    
+#     response = requests.get(url, headers=headers)
+#     if response.status_code == 200:
+#         with open('datasets.csv', 'w') as f:
+#             f.write(response.text)
+#         datasets = dd.read_csv("datasets.csv")
+#         hospitals_series_id = datasets['DataSetId'].compute()
+#         return hospitals_series_id
+#     else:
+#         print("Failed to fetch data. Status code:", response.status_code)
+#         return None
+
+
+# hospitals_series_id = get_hospitals_series_id()
+# if hospitals_series_id is not None:
+#     print(hospitals_series_id)
+
+
+def get_hospitals_selected_id(ReportedMeasureCode, ReportedMeasureName, ReportingStartDate):
+    url = "https://myhospitalsapi.aihw.gov.au/api/v1/datasets/"
+    headers = {
+        'Authorization': 'Bearer YOUR_ACCESS_TOKEN',  
+        'User-Agent': 'MyApp/1.0',
+        'accept' : 'text/csv'
+    }
+    
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        with open('selected_datasets.csv', 'w') as f:
+            f.write(response.text)
+        
+        # Load the CSV file using Dask
+        datasets = dd.read_csv("selected_datasets.csv")
+        
+        # Apply filters
+        filtered_datasets = datasets[
+            (datasets['ReportedMeasureCode'] == ReportedMeasureCode) &
+            (datasets['ReportedMeasureName'] == ReportedMeasureName) &
+            (datasets['ReportingStartDate'] == ReportingStartDate)
+        ]
+        
+        result = filtered_datasets.compute()
+        dataset_ids = result['DataSetId']
+        
+        return dataset_ids
+    else:
+        print("Failed to fetch data. Status code:", response.status_code)
+        return None
+
+hospitals_selected_id = get_hospitals_selected_id("MYH-RM0001", "Breast cancer", "2011-07-01")
+if hospitals_selected_id is not None:
+    print(hospitals_selected_id)
+
+
+def download_datasets(num_datasets_to_download, dataset_id):
+    base_url = "https://myhospitalsapi.aihw.gov.au/api/v1/datasets/"
+    headers = {
+    'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+    'User-Agent': 'MyApp/1.0',
+    'accept': 'text/csv'
+    }
+    for dataset_id in dataset_id[:num_datasets_to_download]:
+        url = f"{base_url}{dataset_id}/data-items"
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
+                temp_file.write(response.text)
+                temp_file_path = temp_file.name
+
+            ddf = dd.read_csv(temp_file_path, dtype={'Caveats': 'object', 'Suppressions': 'object'})
+
+            conn_string = 'postgresql://myuser:mypassword@localhost:5432/mydatabase'
+            engine = create_engine(conn_string)
+
+            conn1 = psycopg2.connect( 
+	            database="mydatabase", 
+                user='myuser', 
+                password='mypassword', 
+                host='localhost', 
+                port= '5432'
+                )
+            conn1.autocommit = True
+            cursor = conn1.cursor() 
+            cursor.execute('drop table if exists prova') 
+            ddf.to_sql('prova', engine, if_exists='replace', index=False)
+            conn1.commit() 
+            conn1.close() 
+            
+        else:
+            print(f"Error fetching dataset with ID {dataset_id}:")
+            print("Status Code:", response.status_code)
+            print("Response Headers:", response.headers)
+            print("Response Text:", response.text)
+
+download_datasets(1, hospitals_selected_id)
+
+
+
+
+#with spark
+
+# def get_hospitals_selected_id(ReportedMeasureCode, ReportedMeasureName, ReportingStartDate):
+#     url = "https://myhospitalsapi.aihw.gov.au/api/v1/datasets/"
+#     headers = {
+#         'Authorization': 'Bearer YOUR_ACCESS_TOKEN',  2
+#         'User-Agent': 'MyApp/1.0',
+#         'accept' : 'text/csv'
+#     }
+    
+#     response = requests.get(url, headers=headers)
+#     if response.status_code == 200:
+#         with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
+#             temp_file.write(response.text)
+#             temp_file_path = temp_file.name
+        
+#         spark = SparkSession.builder.appName("MyHospitals").getOrCreate()
+        
+#         datasets = spark.read.csv(temp_file_path, header=True)
+        
+#         filtered_datasets = datasets.filter(
+#             (datasets['ReportedMeasureCode'] == ReportedMeasureCode) &
+#             (datasets['ReportedMeasureName'] == ReportedMeasureName) &
+#             (datasets['ReportingStartDate'] == ReportingStartDate)
+#         )
+        
+#         dataset_ids = filtered_datasets.select('DatasetId').rdd.flatMap(lambda x: x).collect()
+        
+#         spark.stop()  
+        
+#         return dataset_ids
+#     else:
+#         print("Failed to fetch data. Status code:", response.status_code)
+#         return None
+
+# #example
+# hospitals_selected_id = get_hospitals_selected_id("MYH-RM0001", "Breast cancer", "2011-07-01")
+# if hospitals_selected_id is not None:
+#     print(hospitals_selected_id)
+
+# def download_datasets(num_datasets_to_download, dataset_id):
+#     base_url = "https://myhospitalsapi.aihw.gov.au/api/v1/datasets/"
+#     headers = {
+#         'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+#         'User-Agent': 'MyApp/1.0',
+#         'accept': 'text/csv'
+#     }
+#     for dataset_id in dataset_id[:num_datasets_to_download]:
+#         url = f"{base_url}{dataset_id}/data-items"
+#         response = requests.get(url, headers=headers)
+
+#         if response.status_code == 200:
+#             with open(f"{dataset_id}.csv", "w") as csv_file:
+#                 csv_file.write(response.text)
+#         else:
+#             print(f"Error fetching dataset with ID {dataset_id}:")
+#             print("Status Code:", response.status_code)
+#             print("Response Headers:", response.headers)
+#             print("Response Text:", response.text)
+
+# download_datasets(1, hospitals_selected_id)
+
+
+
--- data_collector/mapper.py
+++ data_collector/mapper.py
@@ -22,4 +22,29 @@ def map_hospitals():
 
     return df
 
-print(map_hospitals().tail())
+data = map_hospitals()
+
+import psycopg2 
+import pandas as pd 
+from sqlalchemy import create_engine 
+
+conn_string = 'postgresql://myuser:mypassword@localhost:5432/mydatabase'
+engine = create_engine(conn_string)
+
+conn1 = psycopg2.connect( 
+	database="mydatabase", 
+    user='myuser', 
+    password='mypassword', 
+    host='localhost', 
+    port= '5432'
+    ) 
+
+conn1.autocommit = True
+cursor = conn1.cursor() 
+
+cursor.execute('drop table if exists prova') 
+data.to_sql('prova', engine, if_exists='replace', index=False)
+
+conn1.commit() 
+conn1.close() 
+
