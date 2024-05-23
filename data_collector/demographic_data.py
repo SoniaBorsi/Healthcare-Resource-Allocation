d16f866d0ad520f47ab8e5e96eaedfd8d95caca3
Sonia Borsi <soniaborsi1101@gmail.com>
2024-05-23 16:07:21 +0200

    updated demographic data

--- data_collector/demographic_data.py
+++ data_collector/demographic_data.py
@@ -2,15 +2,15 @@ import requests
 
 API_key = 'yavM2fmARyaktqwUh9UiL5b4ijS0jVBZ3ZiSlCyG'
 
-base_url = 'https://indicator.api.abs.gov.au/v1/data/CPI_M_H/csv'
+base_url = 'https://api.data.abs.gov.au/dataflow/abs'
 
 headers = {
-    'x-api-key': API_key,
-    'accept' : 'text/csv'
+   # 'x-api-key': API_key,
+    'format' : 'csv'
 }
 
 response = requests.get(base_url, headers=headers)
 
-print(response.text)
+print(response.content)
 
 
