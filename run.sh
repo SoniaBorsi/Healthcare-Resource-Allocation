2a080a279741b21ff87a574406e576f2ed5a16d7
pippotek <filippo.costamagna@studenti.unitn.it>
2024-07-19 16:34:09 +0200

    dashboard container

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 SPARK_MASTER_URL=spark://spark-master:7077
-PROGRAM_PATH=main.py
+PROGRAM_PATH=ETL.py
 
 docker compose up --build -d
 docker compose exec spark-master spark-submit --master $SPARK_MASTER_URL $PROGRAM_PATH
